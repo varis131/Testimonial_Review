bcee07e89462a1dffe2222a032e447d5df46ed37
varis131 <movaris85@gmail.com>
2026-01-17 18:02:58 +0530

    created setup for Testimonial review and added gitignore

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Testimonial from "./Components/Testimonial.jsx";
+import reviews from "./data";
+
+const App = () => {
+  return (
+   <div>
+    App
+   </div>
+  );
+};
+
+export default App;
