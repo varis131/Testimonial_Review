5fcb6e5149cd4a7240251dd5bcf27e2f3decbe6c
varis131 <movaris85@gmail.com>
2026-01-18 18:43:59 +0530

    added tailwind css and button functinality

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,49 +1,40 @@
-import React from 'react';
-import {FaQuoteLeft,FaQuoteRight} from 'react-icons/fa';
+import React from "react";
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
 
-const Card = (props) =>{
-    let review=props.review;
-    return (
-        <div>
-            
-            <div>
-                <img src='review.image' alt=''></img>
-            </div>
+const Card = (props) => {
+  let review = props.review;
+  return (
+    <div className="flex flex-col md:relative">
+      <div className="absolute top-[-7rem] z-[10] mx-auto">
+        <img
+          className="aspect-square rounded-full w-[140px] h-[140px] z-[25] "
+          src={review.image}
+        />
+        <div
+          className="w-[140px] h-[140px] bg-violet-500 rounded-full absolute 
+                top-[-6px] z-[-10] left-[10px]"
+        ></div>
+      </div>
 
-            <div>
-                <p>{review.name}</p>
-            </div>
+      <div className="text-center mt-7">
+        <p className="tracking-wider font-bold text-2xl capitalize">{review.name}</p>
+      </div>
 
-            <div>
-                <p>{review.job}</p>
-            </div>
+      <div className="text-center ">
+        <p className="text-violet-400 uppercase text-sm">{review.job}</p>
+      </div>
 
-            <div>
-                <FaQuoteLeft/>
-            </div>
+      <div className="mx-auto text-violet-400 mt-5  ">
+        <FaQuoteLeft />
+      </div>
 
-            <div>{review.text}</div>
+      <div className="text-center text-slate-500 mt-3">{review.text}</div>
 
-            <div>
-                <FaQuoteRight/>
-            </div>
-
-            <div>
-                <button>+</button>
-                <button>*</button>
-
-            </div>
-
-            <div>
-                <button>Surprise Me</button>
-            </div>
-
-
-
-        </div>
-
-    );
-}
+      <div className="mx-auto text-violet-400 mt-5  ">
+        <FaQuoteRight />
+      </div>
+    </div>
+  );
+};
 
 export default Card;
-
--- src/Components/Testimonial.jsx
+++ src/Components/Testimonial.jsx
@@ -1,13 +1,63 @@
-import React from 'react';
-import Card from './Card.jsx';
+import React, { useState } from "react";
+import Card from "./Card.jsx";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const Testimonial = (props) => {
-  let reviews=props.reviews;
-  return (
-    <div>
-      <Card review={reviews[0]}></Card>
-    </div>
+  let reviews = props.reviews;
+  const [index, setIndex] = useState(0);
 
+  function leftShiftHandler() {
+    if (index - 1 < 0) {
+      setIndex(reviews.length - 1);
+    } else {
+      setIndex(index - 1);
+    }
+  }
+  function rightShiftHandler() {
+    if (index + 1 >= reviews.length) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  }
+  function surpriseHandler() {
+    let randomIndex = Math.floor(Math.random() * reviews.length);
+    setIndex(randomIndex);
+  }
+
+  return (
+    <div
+      className="w-[85vw] md:w-[700px] bg-white flex flex-col justify-center 
+    items-center mt-10 p-10 transition-all duration-700 
+    hover:shadow-xl rounded-md"
+    >
+      <Card review={reviews[index]}></Card>
+      <div className="text-violet-400 flex gap-3 font-bold mt-5 mb-5 mx-auto justify-center ">
+        <button
+          onClick={leftShiftHandler}
+          className="cursor-pointer hover:text-violet-600 text-2xl"
+        >
+          <FiChevronLeft />
+        </button>
+        <button
+          onClick={rightShiftHandler}
+          className="cursor-pointer hover:text-violet-600 text-2xl"
+        >
+          <FiChevronRight />
+        </button>
+      </div>
+
+      <div>
+        <button
+          onClick={surpriseHandler}
+          className="bg-violet-400 hover:bg-violet-600 
+                      transition-all duration-200 cursor-pointer py-2 px-10 
+                      rounded-md font-bold text-white text-lg"
+        >
+          Surprise Me
+        </button>
+      </div>
+    </div>
   );
-}
+};
 export default Testimonial;
